95e6ecad5625b347da27a897c0d6c83df92777ad
xie wen <x826190447@126.com>
2026-05-07 11:05:56 +0800

    add minigame & leanclr

---
+++ Assets/Editor/AutoBuild.cs
@@ -0,0 +1,35 @@
+﻿
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+using WeChatWASM;
+public class AutoBuild
+{
+    
+    [MenuItem("Build/BuildPlatform")]
+    public static void BuildWXMiniGame()
+    {
+        const string CDN = "http://172.16.1.20/";
+        WXConvertCore.config.ProjectConf.Appid = "xxxxxx";
+
+        WXConvertCore.config.ProjectConf.bundleExcludeExtensions = "json;version;";
+
+        var path = string.Format("{0}/../AutoBuild/wx_export", Application.dataPath);
+
+        if(!Directory.Exists(path))
+            Directory.CreateDirectory(path);
+
+        WXConvertCore.config.ProjectConf.relativeDST = path;
+
+        WXConvertCore.config.ProjectConf.CDN = CDN;
+        WXConvertCore.config.CompileOptions.Webgl2 = true;
+
+        EditorUtility.SetDirty(WXConvertCore.config);
+
+        AssetDatabase.SaveAssets();
+
+        AssetDatabase.Refresh();
+
+        WXConvertCore.DoExport();
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
+
+A SoloGame with Unity MiniGame & LeanCLR
+
+about url
+
+https://github.com/focus-creative-games/leanclr4unity
+
 code ref https://github.com/ddabb/soduku
+
+![img2](/pics/1.jpg)
+
+
+Use Build/BuildPlatform to export minigame
